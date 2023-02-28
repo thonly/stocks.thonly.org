b11500f552118c02e599a9a6a7bff834ccbbb3f6
Thon Ly <thonly@heartbank.ceo>
2023-02-27 17:20:55 -0800

    refactor

---
+++ global.mjs
@@ -0,0 +1,6 @@
+export const ORIGIN = window.location.hostname === '127.0.0.1' ? "http://localhost:333/" : "https://railway.thonly.org/";
+export const THON = window.location.hostname === '127.0.0.1' ? "http://127.0.0.1:5503" : "https://i.thonly.org";
+
+// not needed
+export const MOM = window.location.hostname === '127.0.0.1' ? "http://127.0.0.1:5504" : "https://mom.thonly.org";
+export const DAD = window.location.hostname === '127.0.0.1' ? "http://127.0.0.1:5505" : "https://dad.thonly.org";
